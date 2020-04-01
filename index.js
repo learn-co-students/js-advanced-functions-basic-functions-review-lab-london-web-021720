5df377ac23d6753903e8cd088cf5e4f9fb7f4cc8
Amaterasus <skyetay03@googlemail.com>
2020-04-01 11:51:41 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function saturdayFun(activity = "roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office")
+{
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(higlight = "*")
+{
+    return function(phrase = "special")
+    {
+        return `You are ${higlight}${phrase}${higlight}!`
+    }
+}
+
+const Calculator = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y
+}
+
+function actionApplyer(base, arrFunc)
+{
+    let output = base
+    arrFunc.forEach(element => {
+        output = element(output)
+    });
+    return output
+}
